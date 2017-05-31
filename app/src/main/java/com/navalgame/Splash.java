f5271beb32f6e2d9c8de27e3cfb7122a25bb40c4
JU-2094 <urbgon@gmail.com>
2017-05-30 20:18:59 -0500

    Splash added

--- app/src/main/java/com/navalgame/Splash.java
+++ app/src/main/java/com/navalgame/Splash.java
@@ -1,13 +1,33 @@
 package com.navalgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Splash extends AppCompatActivity {
+
+    private static final long SPLASH_SCREEN_DELAY = 20000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.splash_layout);
+
+        getSupportActionBar().hide();
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+//                Intent menu = new Intent(Splash.this,MainMenu.class);
+//                startActivity(menu);
+                finish();
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(task,SPLASH_SCREEN_DELAY);
     }
 }
