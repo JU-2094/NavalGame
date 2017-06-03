92b8dccb8ebf0b6865264c956dd59139dc2b3536
jedy23 <operacionbip@gmail.com>
2017-06-03 16:27:30 -0500

    Cambios

--- app/src/main/java/com/navalgame/MainMenu.java
+++ app/src/main/java/com/navalgame/MainMenu.java
@@ -1,13 +1,34 @@
 package com.navalgame;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends Activity {
+
+    Button play,config,exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+
+
+        play = (Button)findViewById(R.id.play);
+        config = (Button)findViewById(R.id.config);
+        exit = (Button)findViewById(R.id.exit);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                /*
+                Intent play = new Intent(MainMenu.this,Game.class);
+                startActivity(play);*/
+            }
+        });
     }
 }
