d6c7e732620bce9b04366fc48157d29fa391cce8
jedy23 <operacionbip@gmail.com>
2017-06-03 17:47:47 -0500

    Cambios

--- app/src/main/java/com/navalgame/MainMenu.java
+++ app/src/main/java/com/navalgame/MainMenu.java
@@ -2,6 +2,7 @@ package com.navalgame;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +10,7 @@ import android.widget.Button;
 public class MainMenu extends Activity {
 
     Button play,config,exit;
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,17 +19,39 @@ public class MainMenu extends Activity {
 
 
 
-        play = (Button)findViewById(R.id.play);
-        config = (Button)findViewById(R.id.config);
-        exit = (Button)findViewById(R.id.exit);
+        play = (Button)findViewById(R.id.btn_play);
+        config = (Button)findViewById(R.id.btn_config);
+        exit = (Button)findViewById(R.id.btn_exit);
+
+        mp = MediaPlayer.create(this,R.raw.snapper);
 
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                mp.start();
                 /*
                 Intent play = new Intent(MainMenu.this,Game.class);
                 startActivity(play);*/
+            }
+        });
+
+        config.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mp.start();
+            }
+        });
+
+        exit.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+                mp.start();
+                System.exit(0);
+                onDestroy();
+
             }
         });
     }
